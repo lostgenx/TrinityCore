93cf7c0f6329144fdcc170b332acf1eb1ec2f1bf
lostgenx <lostgenerationx@msn.com>
2013-05-08 20:24:45 -0600

    Core/Scripts: Utgarde Pinnacle - Update for Svala and Skadi

--- sql/updates/LostCore/2013_05_07_skadi.sql
+++ sql/updates/LostCore/2013_05_07_skadi.sql
@@ -1,3 +1,17 @@
--- Update InhabitType for Skadi and Grauf. (Very Hacky, It's under the "It Works" Category.)
+-- Update InhabitTypes for Utgarde Pinnacle
+
+-- Skadi and Grauf Normal
 UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=26893;
 UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=26693;
+
+-- Skadi and Grauf Heroic
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=30807;
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=30775;
+
+-- Svala Normal
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=26668;
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=29281;
+
+-- Svala Heroic
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=30809;
+UPDATE `creature_template` SET `InhabitType`=7 WHERE `entry`=30810;
